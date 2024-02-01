96d5032fd8180a07717aaa290364bb2ea884d08b
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-02-02 02:48:12 +0530

    added env

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { log } = require("logggger");
+const { log, warn } = require("logggger");
 const { errorMiddleware } = require("./src/middlewares");
+const { loadEnv } = require("./src/utils");
 
-const port = 5000;
-const hostname = "127.0.0.1";
+const cuurentEnv = loadEnv();
+const port = process.env.PORT || 5000;
+const hostname = process.env.HOSTNAME || "127.0.0.1";
 
 const app = express();
 
@@ -22,6 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 // error middleware
 app.use(errorMiddleware);
 
-app.listen(port, hostname, () => {
-  log(`Server running > http://${hostname}:${port}`);
+app.listen(port, hostname, async () => {
+  warn(`Environment > ${cuurentEnv}`);
+  log(`Server > http://${hostname}:${port}`);
 });
